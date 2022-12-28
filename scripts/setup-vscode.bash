4d82e8ab2691fb81b87e8a674228a907a1f18ed2
hedwig100 <pentagon7951@outlook.jp>
2022-12-28 11:42:55 +0900

    [update] vscodeの設定を変更した

--- scripts/setup-vscode.bash
+++ scripts/setup-vscode.bash
@@ -12,13 +12,13 @@ fi
 VSCODE_SETTING_DIR=""
 if [ "$(uname)" == "Darwin" ]; then
     VSCODE_SETTING_DIR="$HOME/Library/Application Support/Code/User"
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    VSCODE_SETTING_DIR="$HOME/.config/Code/User"
 else
-    echo "Unknown OS"
+    echo "Other than MAC"
     echo "$(uname)"
-    exit 1
 fi
 
-ln -sfv "$XDG_CONFIG_HOME/vscode/settings.json" "$VSCODE_SETTING_DIR/settings.json"
+if [ -n "$VSCODE_SETTING_DIR" ]; then ## mac以外ではsettings.jsonは手動で配置する
+    ln -sfv "$XDG_CONFIG_HOME/vscode/settings.json" "$VSCODE_SETTING_DIR/settings.json"
+fi
+
 bash "$REPO_DIR/config/vscode/extensions.sh"
