b76b64842dcdd0d02027c501294e392fbaae4f46
hedwig100 <pentagon7951@outlook.jp>
2022-12-29 13:15:42 +0900

    [add] package to apt

--- config/apt/install.sh
+++ config/apt/install.sh
@@ -9,6 +9,13 @@ apt-get install -y \
     zlib1g \
     zlib1g-dev \
     libssl-dev \
+    libffi-dev \
+    libbz2-dev \
+    libreadline-dev\
+    libsqlite3-dev \
+    libncursesw5-dev \
+    tk-dev \
+    liblzma-dev \
     wget \
     zip \
     zsh 
